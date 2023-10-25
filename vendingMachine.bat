8e4e195685ecc40bfd29ca2edfd2eda03e047d0e
Emilio San Miguel <lanchas9@yahoo.com.mx>
2023-10-24 19:49:04 -0600

    kia ruleta localmachine

--- vendingMachine.bat
+++ vendingMachine.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --chrome --kiosk http://localhost/kia/ruleta.html --incognito --disable-pinch --no-user-gesture-required --overscroll-history-navigation=0
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --chrome --kiosk http://localhost/kia/configuracion.html --incognito --disable-pinch --no-user-gesture-required --overscroll-history-navigation=0
